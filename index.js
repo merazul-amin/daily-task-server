b8b8daf967c934fa4cca10e8ac7abd7d390c0a60
Merazul Amin Saikut <merazulamin2122@yahoo.com>
2022-12-29 10:40:40 +0600

    task delete api

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000;
 const app = express();
 
@@ -23,10 +23,26 @@ const userTasks = client.db('daily-task').collection('user-tasks');
 
 async function run() {
     //set tasks in db
-
     app.post('/user-task', async (req, res) => {
         const userTask = req.body;
         const result = await userTasks.insertOne(userTask);
+        res.send(result);
+    })
+
+    //get tasks from db by email
+    app.get('/userTasks/:email', async (req, res) => {
+        const email = req.params.email;
+        const query = { userEmail: email };
+        const tasks = await userTasks.find(query).toArray();
+        res.send(tasks);
+    })
+
+
+    //delete task
+    app.delete('/userTasks/:id', async (req, res) => {
+        const id = req.params.id;
+        const query = { _id: ObjectId(id) };
+        const result = await userTasks.deleteOne(query);
         res.send(result);
     })
 
